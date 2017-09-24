0309aacdf5bdb6c8dd844e1b086e38c58a79d267
Dmitriy Otstavnov <tabroot@yandex-team.ru>
2017-09-24 18:27:59 +0300

    Bubble sort implemented

---
+++ homework2/sort_utils.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def swap(array, i, j):
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
---
+++ homework2/utests.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from bubble_sort import sort as bubble_sort
+
+
+class BubbleSort(unittest.TestCase):
+    def test_simple_sorting(self):
+        array = [2, 3, 1]
+        bubble_sort(array)
+        self.assertEqual(array, [1, 2, 3])
+
+    def test_not_positive_numbers(self):
+        array = [100, 0, -10, 4, 1, -54, 9, -100]
+        bubble_sort(array)
+        self.assertEqual(array, [-100, -54, -10, 0, 1, 4, 9, 100])
+
+    def test_sort_sorted_array(self):
+        array = [-10, -5, 0, 5, 10]
+        bubble_sort(array)
+        self.assertEqual(array, [-10, -5, 0, 5, 10])
+
+    def test_sort_empty_array(self):
+        array = []
+        bubble_sort(array)
+        self.assertEqual(array, [])
+
+    def test_sort_one_element_array(self):
+        array = [1]
+        bubble_sort(array)
+        self.assertEqual(array, [1])
+
+    def test_sort_big_arrays(self):
+        array = [-992134, 2135, 61, 0, 215, 213952135, -234, 0, 521, 324, 765, 1337, -10, -100, 52]
+        bubble_sort(array)
+        self.assertEqual(array, [-992134, -234, -100, -10, 0, 0, 52, 61, 215, 324, 521, 765, 1337, 2135, 213952135])
+
+
+if __name__ == '__main__':
+    unittest.main()
