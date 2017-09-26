05444392b4c4ec6e9bb61213f3c55b4ef6cf95dd
Yaroslav <sharko24@yandex.ru>
2017-09-26 13:55:27 +0300

    first commit

---
+++ homework2/selection_sort.py
@@ -0,0 +1,25 @@
+def input_arr(num):
+    arr = []
+    for i in range(0, num):
+        current_elem = float(input('Введите ' + str(i) + ' элемент массива: '))
+        arr.append(current_elem)
+
+    return arr
+
+
+def selection_sort(arr):
+    for i in range(0, len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+
+arr = input_arr(int(input('Введите размер массива: ')))
+print(selection_sort(arr))
+"""""
+Сложность O(n^2)
+Еще один малоэффективный алгоритм сортировки
+Проходит каждый элемент массива и ищет на его место минимальный(максимальный) элемент
+Который ставит меняет местами с текущим элементом массива
+"""""
