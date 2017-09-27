3bb6e70741a9c728c10c8a9fb343158452ca563f
unknown <elizaveta-lazban@mail.ru>
2017-09-28 00:11:44 +0300

    last commit

--- homework2/insertion_sort.py
+++ homework2/insertion_sort.py
@@ -1,13 +1,14 @@
-def insertion_sort(arrayToSort):
-	a = arrayToSort
-	for i in range(len(a)):
-		k = a[i]
-		j = i
-		while (a[j - 1] > k) and (j > 0):
-			a[j] = a[j - 1]
-			j = j - 1
-		a[j] = k
-	return a
+def insertion_sort(array):
+    a = array
+    for i in range(len(a)):
+        k = a[i]
+        j = i
+        while (a[j - 1] > k) and (j > 0):
+            a[j] = a[j - 1]
+            j = j - 1
+        a[j] = k
+    return a
+
 
 arr = [54, 0, 11, 22, 1, 7, 5, 3, 4, 6, 2]
 print(insertion_sort(arr))
@@ -15,5 +16,4 @@ print(insertion_sort(arr))
 
 # из массива последовательно берется каждый элемент
 # и вставляется в отсортированную часть(например в начале массива или конце)
-#
 # Сложность O(n ** 2)
