3b21d122576df6608334dbd64ecbb9109971821b
nerkeskulbaeva <nerkeskulbaeva@gmail.com>
2017-09-28 21:24:04 +0300

    hw/ver4

--- homework2/selection_sort.py
+++ homework2/selection_sort.py
@@ -1,17 +1,18 @@
 # Сложность O(n * n)
 # Сортировка выбором заключается в том, что мы сначала
 #  ищем минимальный элемент массива и кладем в первую ячейку
-# массива, находим след.минимальный элемент и кладем во вторую ячейку и т.д
+# массива, находим след.минимальный элемент
+#  и кладем во вторую ячейку и т.д
 
 
 def selection_sort(array):
     for i in range(len(array)):
-        min = i
+        mini = i
         for j in range(i + 1, len(array)):
-            if array[j] < array[min]:
-                min = j
-        t = array[min]
-        array[min] = array[i]
+            if array[j] < array[mini]:
+                mini = j
+        t = array[mini]
+        array[mini] = array[i]
         array[i] = t
     return array
 
