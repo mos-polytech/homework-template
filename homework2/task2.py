e772c675e625e44c01c20f75f2469b8330154cce
nikita <v.nik-97@bk.ru>
2017-09-28 13:46:05 +0300

    fix#1

--- homework2/task2.py
+++ homework2/task2.py
@@ -2,15 +2,15 @@ s = [2, 0, 2, -1, 9]
 k = 0
 
 while k < len(s) - 1:
-    min = k
+    mm = k
     i = k + 1
     while i < len(s):
-        if s[i] < s[min]:
+        if s[i] < s[mm]:
             m = i
         i += 1
     t = s[k]
-    s[k] = s[min]
-    s[min] = t
+    s[k] = s[mm]
+    s[mm] = t
     k += 1
 
 print(s)
