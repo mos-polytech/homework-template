ed94cdc53c02cb1116c70e432cc0cbf990235df4
Kaktysig <Kaktysig@MacBook-Air-Kaktys.local>
2017-09-28 15:48:22 +0300

    Change bubble sort

--- homework2/bubble_sort.py
+++ homework2/bubble_sort.py
@@ -1,13 +1,13 @@
 # Сортировка пузырьком
 massive = [4, 3, 2, -5, 3, 8, 32, 54, 83, -23, 43, ]
 
-for i in range(0,len(massive)):
-    for j in range(0,len(massive)-i-1):
-        if massive[j]>massive[j+1]:
-            k=massive[j+1]
-            massive[j+1]=massive[j]
-            massive[j]=k
-print(massive);
+for i in range(0, len(massive)):
+    for j in range(0, len(massive)-i-1):
+        if massive[j] > massive[j+1]:
+            k = massive[j+1]
+            massive[j+1] = massive[j]
+            massive[j] = k
+print(massive)
 
 # Сложность данной сортировка O(n**2)
 # Так как внешний массив проходит n итераций
