51b85637698801eb5afc21f96093ff076b22be58
NickVazovsky <nvazovsky@gmail.com>
2017-10-05 12:23:38 +0300

    homework3

--- homework3/game.py
+++ homework3/game.py
@@ -1,83 +1,77 @@
 # -*- coding: utf-8 -*-
 
-# `random` module is used to shuffle field, see:
-# https://docs.python.org/3/library/random.html#random.shuffle
 import random
 
-# Empty tile, there's only one empty cell on a field:
-EMPTY_MARK = 'x'
+import sys
 
-# Dictionary of possible moves if a form of:
-# key -> delta to move the empty tile on a field.
-MOVES = {
-    'w': -4,
-    's': 4,
-    'a': -1,
-    'd': 1,
+EMPTY_MARK = 'x' #переменная с х
+MOVES = { # задаем движение
+'w' : -4,
+'s' : 4,
+'a' :  -1,
+'d' : 1,
 }
-
-
 def shuffle_field():
-    """
-    This method is used to create a field at the very start of the game.
-    :return: list with 16 randomly shuffled tiles,
-    one of which is a empty space.
-    """
-    pass
-
-
-def print_field(field):
-    """
-    This method prints field to user.
-    :param field: current field state to be printed.
-    :return: None
-    """
-    pass
-
-
-def is_game_finished(field):
-    """
-    This method checks if the game is finished.
-    :param field: current field state.
-    :return: True if the game is finished, False otherwise.
-    """
-    pass
-
-
-def perform_move(field, key):
-    """
-    Moves empty-tile inside the field.
-    :param field: current field state.
-    :param key: move direction.
-    :return: new field state (after the move).
-    :raises: IndexError if the move can't me done.
-    """
-    pass
-
-
+    number_for_games = [] #создаем массив
+    for i in range(1,16):#включаем цикл от 1 до 16
+        number_for_games.append(i) #добавляем  в конец массива
+    number_for_games.append(EMPTY_MARK) # добавляем в конец
+    random.shuffle(number_for_games) # перемешиваем значения
+    return number_for_games #возвращаем значени
+def print_field(number_for_games):
+    j = 0 # задаем цикл
+    while j<len(number_for_games): #Задаем цикл где J меньше массива numbers_for games
+        print(number_for_games[j],number_for_games[j+1],number_for_games[j+2],number_for_games[j+3]) #Печатаем матрицу 4 на 4
+        j+=4
+def is_game_finished(number_for_games): #игра закончена
+    # number_for_games.index('x')
+    wins_checks = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,'x'] # выйгрышная комбинация
+    if number_for_games == [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,'x'] : #  если массив равен выйграшной комбинации
+        return  True # возвращаем тру
+    elif number_for_games == wins_checks: # Если иначе равен
+        return True
+    else:
+        return False
+def perform_move(number_for_games,keys):
+    move_key = MOVES[keys]
+    index = number_for_games.index('x')
+    if ((index >= 0 and index < 4 and keys == 'w') or ((index % 4 == 0 or index == 0) and keys == 'a')):
+        print('Выход за  границы')
+    elif  (index >= 12 and index < 16 and keys == 's') or((index + 1) % 4 == 0 and keys == 'd'):
+        print('Выход за боковые границы')
+    else:
+         location = number_for_games.index('x')
+         move_key = MOVES[keys]
+         number_for_games[location], number_for_games[location + move_key] = number_for_games[location + move_key],number_for_games[location]
+    return number_for_games
 def handle_user_input():
-    """
-    Handles user input. List of accepted moves:
-        'w' - up,
-        's' - down,
-        'a' - left,
-        'd' - right
-    :return: <str> current move.
-    """
-    pass
-
-
+    moving = input('Введите значение:')
+    if moving in MOVES or moving == 'cheat':
+        return moving
+    if moving in MOVES or moving == KeyboardInterrupt:
+        return moving
+    else:
+        return False
 def main():
-    """
-    The main method. It stars when the program is called.
-    It also calls other methods.
-    :return: None
-    """
-    pass
-
-
-if __name__ == '__main__':
-    # See what this means:
-    # http://stackoverflow.com/questions/419163/what-does-if-name-main-do
-
-    main()
+    result_number = shuffle_field()
+    print_field(result_number)
+    while is_game_finished(result_number)==False:
+        handle= handle_user_input()
+        if handle and handle !='END' and handle != 'cheat':
+            result_number = perform_move(result_number,handle)
+            print_field(result_number)
+        elif handle=='END':
+            print('shutting down')
+            break
+        elif handle == 'cheat':
+            cheat = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,'x']
+            if(is_game_finished(cheat)):
+                print('Вы воспользовались читом')
+                print_field(cheat)
+                break
+        else:
+            print('Неверные данные попробуйте еще раз:')
+    while is_game_finished(result_number)==True:
+        print('Поздравляем вы выиграли:')
+        break
+main()
