1a34ff2a02a8bca499a0320701acc6a09f092637
Nikita Vasilyev <v.nik-97@bk.ru>
2017-10-09 14:46:17 +0300

    homework

---
+++ homework1/task1.py
@@ -0,0 +1,5 @@
+print('Введите длину и ширину.')
+h = float(input())
+s = float(input())
+sh = h * s
+print('Площадь прямоугольника: ', sh)
---
+++ homework1/task3.py
@@ -0,0 +1,11 @@
+f = int(input("Введите число: "))
+row = []
+
+for i in range(2, f+1):
+    for j in row:
+        if i % j == 0:
+            break
+    else:
+        row.append(i)
+
+print (row)
