63861fbb79041a13227f3e10593f101d1f350f74
Nikita Vasilyev <v.nik-97@bk.ru>
2017-10-09 14:57:38 +0300

    some fix

--- homework1/task3.py
+++ homework1/task3.py
@@ -1,11 +1,11 @@
-f = int(input("Введите число: "))
+f = int(input('Введите число: '))
 row = []
 
-for i in range(2, f+1):
+for i in range(2, f + 1):
     for j in row:
         if i % j == 0:
             break
-    else:
-        row.append(i)
+        else:
+            row.append(i)
 
-print (row)
+print(row)
--- homework1/task4.py
+++ homework1/task4.py
@@ -1,9 +1,9 @@
-f = int(input("Введите число: "))
-s = int(input("Введите число: "))
+f = int(input('Введите число: '))
+s = int(input('Введите число: '))
 r = []
 
-for i in range(f,s+1):
-    if i%5==0:
-	    r.append(i)
+for i in range(f, s + 1):
+    if i % 5 == 0:
+        r.append(i)
 
 print(r)
